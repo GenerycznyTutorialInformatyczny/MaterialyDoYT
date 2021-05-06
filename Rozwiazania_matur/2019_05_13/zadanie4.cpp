9723831362420d419a987c035ace7a4e864c9af3
GenerycznyTutorialInformatyczny <83618529+GenerycznyTutorialInformatyczny@users.noreply.github.com>
2021-05-06 17:18:18 +0200

    Rename Maturki/Rozwiazania/2019_05_13/zadanie4.cpp to Rozwiazania_matur/2019_05_13/zadanie4.cpp

--- Rozwiazania_matur/2019_05_13/zadanie4.cpp
+++ Rozwiazania_matur/2019_05_13/zadanie4.cpp
@@ -26,7 +26,7 @@ bool is_power_of_3v2(unsigned int x){
 bool is_power_of_3v3(unsigned int x){
 	if(x == 0) return false;
 	if(x == 1) return true;
-	// x == 3 jest ju¿ niepotrzebne, poniewa¿ zostanie to efektywnie sprawdzone w drugin wykonaniu pêtli
+	// x == 3 jest juÅ¼ niepotrzebne, poniewaÅ¼ zostanie to efektywnie sprawdzone w drugin wykonaniu pÄ™tli
 	unsigned int prev_i = x*3;
 	for(unsigned int i = x; i>=1; i/=3){
 		if(prev_i != i*3) return false;
@@ -54,8 +54,8 @@ bool is_equal_to_sum_of_factorials(unsigned int x){
 	return sum == x;
 }
 
-// Jest udowodnione, ¿e wspólny podzielnik a i b jest te¿ podzielnikiem a%b
-// Liczymy takie a%b i b%a na zmianê, redukuj¹c na zmianê liczby a i b do ich reszt z 
+// Jest udowodnione, Å¼e wspÃ³lny podzielnik a i b jest teÅ¼ podzielnikiem a%b
+// Liczymy takie a%b i b%a na zmianÄ™, redukujÄ…c na zmianÄ™ liczby a i b do ich reszt z 
 // dzielenia przez siebie
 //
 // Najpierw mamy a i b
@@ -70,8 +70,8 @@ unsigned int GCD(unsigned int a, unsigned int b){
 		unsigned int b = c;
 	}
 }
-// Mo¿na to uproœciæ do rekurencji
-// Na której nawet wiêcej lepiej jak to dzia³a
+// MoÅ¼na to uproÅ›ciÄ‡ do rekurencji
+// Na ktÃ³rej nawet wiÄ™cej lepiej jak to dziaÅ‚a
 unsigned int GCD_rec(unsigned int a, unsigned int b){
 	if(b!=0) return GCD_rec(b, a%b);
 	return a;
@@ -102,25 +102,25 @@ int main(){
 		
 		//Zadanie 3
 		z3.push_back(a);
-		// Niestety nie da siê w miejscu tego zrobiæ
+		// Niestety nie da siÄ™ w miejscu tego zrobiÄ‡
 		// 10 10 30 30 30 60 3 3 3 6 12
-		// Najd³u¿sza sekwencja powinna byæ 30 - 12
-		// "Przelatuj¹c" w miejscu otrzymalibyœmy sekwencjê 10 - 60
-		// trzeba by by³o operuj¹c na buforze wracaæ za ka¿dym i sprawdzaæ
-		// inne mo¿liwoœci. Lub utworzyæ tablicê tylko obecnie rozpatrywanej sekwencji 		
-		// i po³¹czyæ jakoœ podejœcie w miejscu z tablicami
-		// jendak lepiej sobie ¿ycia w takim zadaniu nie utrudniaæ
-		// (wiêcej przy samym kodzie)
+		// NajdÅ‚uÅ¼sza sekwencja powinna byÄ‡ 30 - 12
+		// "PrzelatujÄ…c" w miejscu otrzymalibyÅ›my sekwencjÄ™ 10 - 60
+		// trzeba by byÅ‚o operujÄ…c na buforze wracaÄ‡ za kaÅ¼dym i sprawdzaÄ‡
+		// inne moÅ¼liwoÅ›ci. Lub utworzyÄ‡ tablicÄ™ tylko obecnie rozpatrywanej sekwencji 		
+		// i poÅ‚Ä…czyÄ‡ jakoÅ› podejÅ›cie w miejscu z tablicami
+		// jendak lepiej sobie Å¼ycia w takim zadaniu nie utrudniaÄ‡
+		// (wiÄ™cej przy samym kodzie)
 		
 	}
 	
 	
 	
 	unsigned int cur_sequence_GCD = GCD_rec(z3[0], z3[1]);
-	unsigned int cur_sequence_len = 2; // zaczynamy od 2, poniewa¿ teoretycznie mamy ju¿ dwa elementy, 0 i 1.
-										// Je¿eli ich NWD jest jednak równy 1, w pêtli jest else który poszukuje dwóch faktycznych
-										// pierwszych elementów pierwszego ci¹gu (czyli dwa pierwsze elementy wystêpuj¹ce w pliku któych
-										// NWD jest wiêkszy od 1)
+	unsigned int cur_sequence_len = 2; // zaczynamy od 2, poniewaÅ¼ teoretycznie mamy juÅ¼ dwa elementy, 0 i 1.
+										// JeÅ¼eli ich NWD jest jednak rÃ³wny 1, w pÄ™tli jest else ktÃ³ry poszukuje dwÃ³ch faktycznych
+										// pierwszych elementÃ³w pierwszego ciÄ…gu (czyli dwa pierwsze elementy wystÄ™pujÄ…ce w pliku ktÃ³ych
+										// NWD jest wiÄ™kszy od 1)
 	unsigned int cur_sequence_beg_numb = z3[0];
 	
 	unsigned int long_sequence_GCD = 0;
@@ -129,57 +129,57 @@ int main(){
 	
 	int i = 2;
 	while(i < z3.size()){
-		if(cur_sequence_GCD != 1){ // sytuacja, kiedy pierwsze dwa elementy maj¹ podzielnik wiêkszy ni¿ 1
+		if(cur_sequence_GCD != 1){ // sytuacja, kiedy pierwsze dwa elementy majÄ… podzielnik wiÄ™kszy niÅ¼ 1
 			unsigned int this_it_GCD = GCD_rec(cur_sequence_GCD, z3[i]);
-			if(this_it_GCD == cur_sequence_GCD){ // kontynacja ci¹gu
+			if(this_it_GCD == cur_sequence_GCD){ // kontynacja ciÄ…gu
 				cur_sequence_len++;
 			}
-			else{ //ci¹g siê skoñczy³
-				if(cur_sequence_len>long_sequence_len){ // je¿eli obecny ci¹g jest tym najd³u¿szym
+			else{ //ciÄ…g siÄ™ skoÅ„czyÅ‚
+				if(cur_sequence_len>long_sequence_len){ // jeÅ¼eli obecny ciÄ…g jest tym najdÅ‚uÅ¼szym
 					long_sequence_GCD = cur_sequence_GCD;
 					long_sequence_len = cur_sequence_len;
 					long_sequence_beg_numb = cur_sequence_beg_numb;
 				}
-				cur_sequence_len = 2; // przede wszystkim ustawiamy 2, poniewa¿ niezale¿ne jak 
-				// now¹ sekwencjê zaczniemy, jej wyjœciowa d³ugoœæ bêdzie równa 2
+				cur_sequence_len = 2; // przede wszystkim ustawiamy 2, poniewaÅ¼ niezaleÅ¼ne jak 
+				// nowÄ… sekwencjÄ™ zaczniemy, jej wyjÅ›ciowa dÅ‚ugoÅ›Ä‡ bÄ™dzie rÃ³wna 2
 				
 				
-				cur_sequence_GCD = 	GCD_rec(z3[i], z3[i-1]);// Muszê sprawdziæ czy element poprzedni 
-				// z obcnym nie maj¹ wspólnych dzielników innych ni¿ jeden:
+				cur_sequence_GCD = 	GCD_rec(z3[i], z3[i-1]);// MuszÄ™ sprawdziÄ‡ czy element poprzedni 
+				// z obcnym nie majÄ… wspÃ³lnych dzielnikÃ³w innych niÅ¼ jeden:
 				// 10 10 30 30 30 60 3 6 6 6 12
-				// Jesteœmy na 3.
-				// Sprawdzamy 3 i 60 ma wspólny dzielnik 3
-				// Cofamy siê a¿ do 30
-				// i póŸniej idziemy od 3 w prawo do 12
+				// JesteÅ›my na 3.
+				// Sprawdzamy 3 i 60 ma wspÃ³lny dzielnik 3
+				// Cofamy siÄ™ aÅ¼ do 30
+				// i pÃ³Åºniej idziemy od 3 w prawo do 12
 				
 				
 				// 
 				if(cur_sequence_GCD != 1){
-					cur_sequence_beg_numb = z3[i-1]; // ustawiam nowy mo¿liwy pocz¹tek sekwencji
+					cur_sequence_beg_numb = z3[i-1]; // ustawiam nowy moÅ¼liwy poczÄ…tek sekwencji
 					
-					int j = i-2; // wiemy ¿e obecny i poprzedni maj¹ jakiœ wspólny dzielnik inny ni¿ 
-					// w³aœnie ukoñczona sekwencja, wiêc wystarczy ¿eby iœæ od i-2
+					int j = i-2; // wiemy Å¼e obecny i poprzedni majÄ… jakiÅ› wspÃ³lny dzielnik inny niÅ¼ 
+					// wÅ‚aÅ›nie ukoÅ„czona sekwencja, wiÄ™c wystarczy Å¼eby iÅ›Ä‡ od i-2
 					
 					this_it_GCD = GCD_rec(cur_sequence_GCD, z3[j]);
 					
 					while(this_it_GCD != cur_sequence_GCD and j > 0){
-						// je¿eli kolejne poprzednie liczby te¿ maj¹ ten sam dzielnik to sekwencjê 
-						// trzeba wyd³u¿aæ "w ty³"
+						// jeÅ¼eli kolejne poprzednie liczby teÅ¼ majÄ… ten sam dzielnik to sekwencjÄ™ 
+						// trzeba wydÅ‚uÅ¼aÄ‡ "w tyÅ‚"
 						cur_sequence_beg_numb = z3[j];
 						j--;
 						this_it_GCD = GCD_rec(cur_sequence_GCD, z3[j]);
 						cur_sequence_len++;
 					}
-					// Teraz g³owna pêtla while zabierze nas dalej "w prawo" od i
+					// Teraz gÅ‚owna pÄ™tla while zabierze nas dalej "w prawo" od i
 					
 				}
-				else{ // je¿eli nie ma sensu siê cofaæ:
-					// przede wszystkim trzeba pamietaæ ¿e nawet je¿eli w "œrodku" obecnie znalezionej
-					// sekwencji by³aby inna sekwencja to nie ma sensu po ni¹ "iœæ" do ty³u bo bêdzie 
-					// ona i tak krótsza od obecnej:
+				else{ // jeÅ¼eli nie ma sensu siÄ™ cofaÄ‡:
+					// przede wszystkim trzeba pamietaÄ‡ Å¼e nawet jeÅ¼eli w "Å›rodku" obecnie znalezionej
+					// sekwencji byÅ‚aby inna sekwencja to nie ma sensu po niÄ… "iÅ›Ä‡" do tyÅ‚u bo bÄ™dzie 
+					// ona i tak krÃ³tsza od obecnej:
 					// 10 20 40 800 800 40 20 10
 					
-					// sytuacja w sumie identyczna jak else pod spodem. Trzeba jakoœ zacz¹æ now¹ sekwencjê
+					// sytuacja w sumie identyczna jak else pod spodem. Trzeba jakoÅ› zaczÄ…Ä‡ nowÄ… sekwencjÄ™
 					cur_sequence_GCD = 	GCD_rec(z3[i], z3[i+1]);
 					cur_sequence_beg_numb = z3[i];
 					
@@ -187,11 +187,11 @@ int main(){
 			}
 			
 		}
-		else{ // sytuacja, kiedy pierwsze dwa elementy nie maj¹ podzielnika wiêkszego ni¿ 1
+		else{ // sytuacja, kiedy pierwsze dwa elementy nie majÄ… podzielnika wiÄ™kszego niÅ¼ 1
 		 	// inaczej:
-			// else który poszukuje pierwsz¹ sytuacjê, kiedy NWD bêdzie wiêkszy od 1
-			// Ponownie kod moo¿e wejœæ tutaj, kiedy zakoñczy siê sekwencja i bêdzie poszukiwany
-			// nowy pocz¹tek sekwencji
+			// else ktÃ³ry poszukuje pierwszÄ… sytuacjÄ™, kiedy NWD bÄ™dzie wiÄ™kszy od 1
+			// Ponownie kod mooÅ¼e wejÅ›Ä‡ tutaj, kiedy zakoÅ„czy siÄ™ sekwencja i bÄ™dzie poszukiwany
+			// nowy poczÄ…tek sekwencji
 			cur_sequence_GCD = GCD_rec(z3[i-1], z3[i]);
 			cur_sequence_beg_numb = z3[i-1];
 		}		
@@ -199,7 +199,7 @@ int main(){
 		i++;		
 	}
 	
-	if(cur_sequence_len>long_sequence_len){ // sprawdzenie czy ostatnia sekwencja nie jest t¹ najd³u¿sz¹
+	if(cur_sequence_len>long_sequence_len){ // sprawdzenie czy ostatnia sekwencja nie jest tÄ… najdÅ‚uÅ¼szÄ…
 		long_sequence_GCD = cur_sequence_GCD;
 		long_sequence_len = cur_sequence_len;
 		long_sequence_beg_numb = cur_sequence_beg_numb;
